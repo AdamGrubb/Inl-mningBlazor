2a89c7bd67793d37bd8f8bb755065b1ea9c58d60
Adam Grubb <adam.grubb.63@hotmail.com>
2023-02-19 20:28:16 +0100

    6-1

---
+++ HorrorFlux.Membership.UI/Program.cs
@@ -0,0 +1,14 @@
+using HorrorFlux.Common.HttpClients;
+using HorrorFlux.Membership.UI;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddHttpClient<MembershipHttpClient>(client =>
+client.BaseAddress = new Uri("https://localhost:6001/api/"));
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
